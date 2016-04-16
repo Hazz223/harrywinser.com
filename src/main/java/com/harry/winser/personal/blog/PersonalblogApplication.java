7f4e57a0ca5d9aa025a0f8e13d8a608d8953e190
Harry Winser <harrywinser@hotmail.co.uk>
2016-04-16 18:27:04 +0100

    Initial commit!

---
+++ src/main/java/com/harry/winser/personal/blog/PersonalblogApplication.java
@@ -0,0 +1,12 @@
+package com.harry.winser.personal.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalblogApplication.class, args);
+	}
+}
