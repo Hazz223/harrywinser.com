1c231deb01f055a7ae899609b26bd3b0c23a850e
Harry Winser <harrywinser@hotmail.co.uk>
2016-04-17 20:17:53 +0100

    Continued to work on the front end.

--- src/main/java/com/harry/winser/personal/blog/Application.java
+++ src/main/java/com/harry/winser/personal/blog/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonalblogApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersonalblogApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
