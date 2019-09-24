a3555869240abce6795293f3e75dbc9aaa534f39
Harry <harrywinser@hotmail.co.uk>
2019-09-24 18:37:50 +0100

    Removed pointless line

--- web/index.jsx
+++ web/index.jsx
@@ -3,6 +3,5 @@ import React from "react";
 import App from "./App.jsx";
 import '../web/style.css';
 
-
 const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(<App />, wrapper) : false;
